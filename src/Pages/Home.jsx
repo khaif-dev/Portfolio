4089851209334a213e8d27c37aad4760b6db3da9
AVGDRS <immafukwo@gmail.com>
2025-12-16 08:56:33 +0300

    changes text color on hover

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -48,7 +48,8 @@ const Home = () => {
           {/* Action Buttons */}
           <div className="flex flex-wrap justify-center gap-4">
             <Link to="/">
-              <Button className="bg-black text-white dark:bg-white/90 dark:text-black p-6 text-lg shadow-lg hover:bg-white/60 transition-all duration-300">
+              <Button className="bg-black text-white dark:bg-white/90 dark:text-black p-6 text-lg shadow-lg 
+              hover:bg-white/60 hover:text-black transition-all duration-300">
                 <Mail className="w-5 h-5 mr-2" />
                 Get In Touch
               </Button>
